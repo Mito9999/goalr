fc0d422c382cf3222b21ad35caa63fcdde2f5198
Mito6211 <willmoore674@gmail.com>
2021-10-17 11:28:40 -0400

    Refactored properly & re-add Modal

--- pages/app.js
+++ pages/app.js
@@ -1,19 +1,21 @@
 import Head from "next/head";
-import { LineChart, Line, YAxis, ResponsiveContainer } from "recharts";
+import { LineChart, Line, YAxis } from "recharts";
 import { MdRefresh, MdAdd, MdSettings } from "react-icons/md";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getAuth } from "firebase/auth";
 import { seedDb } from "../firebase/clientApp";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 import Modal from "../components/Modal";
+import Spinner from "../components/Spinner";
 
 export default function Home() {
-  const [showModal, setShowModal] = useState(false);
   const [user, isUserLoading] = useAuthState(getAuth());
   const [cards, setCards] = useState([]);
   const [isLoadingCards, setIsLoadingCards] = useState(false);
   const [userHasGoals, setUserHasGoals] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [cardInModal, setCardInModal] = useState({});
 
   useEffect(() => {
     if (user) {
@@ -39,54 +41,11 @@ export default function Home() {
     console.log(user);
   }, [user]);
 
-  return (
-    <div className="flex flex-col max-w-cardContainer mx-auto px-3 font-inter">
-      <Head>
-        <title>Goalr App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <div className="my-4 pl-6">
-          {user ? (
-            <h2 className="text-xl opacity-50">
-              Welcome Back, {user.displayName}!
-            </h2>
-          ) : (
-            <h2 className="text-xl opacity-50">Log in to save your goals!</h2>
-          )}
-
-          <h1 className="text-3xl font-bold flex">
-            Goals{" "}
-            {isLoadingCards && (
-              <svg
-                className="animate-spin h-5 w-5 ml-4 my-auto text-black"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            )}
-          </h1>
-        </div>
-        <div className="flex flex-col max-w-cardContainer mb-8">
-          {!userHasGoals ? (
-            <div className="bg-gray-50 my-3 rounded-md p-8 border border-gray-200 flex justify-center items-center text-2xl">
-              You haven't set any goals yet. Click below to get started!
-            </div>
-          ) : (
+  const CardList = useMemo(
+    () => () =>
+      (
+        <>
+          {cards.length > 0 ? (
             cards.map((card) => (
               <div
                 key={card.title}
@@ -147,7 +106,10 @@ export default function Home() {
                     )}
                     <button
                       className="text-lg text-white bg-blue-600 rounded-md ml-2 p-3.5"
-                      onClick={() => setShowModal(true)}
+                      onClick={() => {
+                        setCardInModal(card);
+                        setShowModal(true);
+                      }}
                     >
                       <MdSettings className="h-6 w-6" />
                     </button>
@@ -166,6 +128,137 @@ export default function Home() {
                 </div>
               </div>
             ))
+          ) : (
+            <div></div>
+          )}
+        </>
+      ),
+    [cards]
+  );
+
+  const EditModal = useMemo(
+    () => () =>
+      (
+        <Modal
+          onClose={() => setShowModal(false)}
+          show={showModal}
+          title={cardInModal.title}
+        >
+          <div
+            onChange={(e) =>
+              setCardInModal((prev) => ({
+                ...prev,
+                isManualEntry: e.target.value === "manual" ? true : false,
+              }))
+            }
+            className="my-4"
+          >
+            <h3 className="font-bold mb-2">Type</h3>
+
+            <input
+              type="radio"
+              id="card-type-manual"
+              name="card-type"
+              value="manual"
+              className="mr-1.5"
+              defaultChecked={cardInModal.isManualEntry}
+            />
+            <label htmlFor="card-type-manual">Manual</label>
+
+            <input
+              type="radio"
+              id="card-type-auto"
+              name="card-type"
+              value="auto"
+              className="ml-4 mr-1.5"
+              defaultChecked={!cardInModal.isManualEntry}
+            />
+            <label htmlFor="card-type-auto">Auto</label>
+          </div>
+          <div className="mb-4">
+            <h3 className="font-bold mb-2">Goal</h3>
+
+            <input
+              type="number"
+              value={cardInModal.goal}
+              className="border-2 border-gray-200 rounded-md p-2 w-60"
+              onChange={(e) =>
+                setCardInModal((prev) => ({
+                  ...prev,
+                  goal: Number(e.target.value),
+                }))
+              }
+            />
+          </div>
+          <div className="mb-4">
+            <h3 className="font-bold mb-2">Units</h3>
+
+            <input
+              type="text"
+              value={cardInModal.units}
+              className="border-2 border-gray-200 rounded-md p-2 w-60"
+              onChange={(e) =>
+                setCardInModal((prev) => ({ ...prev, units: e.target.value }))
+              }
+            />
+          </div>
+          <div className="mb-4">
+            <h3 className="font-bold mb-2">Inspiration</h3>
+            <textarea
+              value={cardInModal.inspiration}
+              onChange={(e) =>
+                setCardInModal((prev) => ({
+                  ...prev,
+                  inspiration: e.target.value,
+                }))
+              }
+              className="w-60 p-2 border-2 border-gray-200 rounded-md"
+            />
+          </div>
+          <button
+            className="text-lg text-white bg-blue-600 px-8 py-3 rounded-md w-full"
+            onClick={() => {
+              const newCards = [...cards];
+              const oldCardIndex = newCards.findIndex(
+                (card) => card.id === cardInModal.id
+              );
+              newCards[oldCardIndex] = { ...cardInModal };
+              setCards(newCards);
+              setShowModal(false);
+            }}
+          >
+            Done
+          </button>
+        </Modal>
+      ),
+    [showModal, cardInModal, cards]
+  );
+
+  return (
+    <div className="flex flex-col max-w-cardContainer mx-auto px-3 font-inter">
+      <Head>
+        <title>Goalr App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div className="my-4 pl-6">
+          <h2 className="text-xl opacity-50">
+            {user
+              ? `Welcome Back, ${user.displayName}!`
+              : "Log in to save your goals!"}
+          </h2>
+          <h1 className="text-3xl font-bold flex">
+            Goals <Spinner loading={isLoadingCards} />
+          </h1>
+        </div>
+
+        <div className="flex flex-col max-w-cardContainer mb-8">
+          {!userHasGoals ? (
+            <div className="bg-gray-50 my-3 rounded-md p-8 border border-gray-200 flex justify-center items-center text-2xl">
+              You haven't set any goals yet. Click below to get started!
+            </div>
+          ) : (
+            <CardList />
           )}
           {!isLoadingCards && user && (
             <div
@@ -179,21 +272,7 @@ export default function Home() {
           )}
         </div>
       </main>
-      <Modal
-        onClose={() => setShowModal(false)}
-        show={showModal}
-        title="Modal Title"
-      >
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </Modal>
+      {cardInModal && <EditModal />}
     </div>
   );
 }
